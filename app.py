a0fffaf9e45b61b1fa51c4502bdc85117c7a213f
mtto <mjhmje87@gmail.com>
2023-02-12 05:41:40 +0900

    app.py 모두 합친 버전

--- app.py
+++ app.py
@@ -53,6 +53,73 @@ for trabucket in trabuckets:
 def main():
     return render_template('main.html')
 
+@app.route('/sub')
+def sub():
+    return render_template('sub.html')
+
+#각 상세페이지들 만들기
+@app.route('/sub/<code>')
+def view(code):
+    return render_template('sub.html')
+
+@app.route("/bucket", methods=["POST"])
+def bucket_post():
+    bucket_receive = request.form['bucket_give']
+    code_receive = request.form['code_give']
+    bucket_list = list(db.bucket.find({}, {'_id': False}))
+    if len(bucket_list) == 0:
+        count = 1
+        doc = {
+            'code': code_receive,
+            'num': count,
+            'bucket': bucket_receive,
+            'done': 0
+        }
+        db.bucket.insert_one(doc)
+        return jsonify({'msg': '등록 완료'})
+    else:
+        count = db.bucket.find_one(sort=[("num", -1)])["num"] + 1
+        doc = {
+            'code': code_receive,
+            'num': count,
+            'bucket': bucket_receive,
+            'done': 0
+        }
+        db.bucket.insert_one(doc)
+        return jsonify({'msg': '등록 완료'})
+
+@app.route("/bucket", methods=["GET"])
+def bucket_get():
+    bucket_list = list(db.bucket.find({}, {'_id': False}))
+    return jsonify({'buckets': bucket_list})
+
+#버킷 완료했을 때
+@app.route("/bucket/done", methods=["POST"])
+def bucket_done():
+    num_receive = request.form['num_give']
+    db.bucket.update_one({'num': int(num_receive)}, {'$set': {'done': 1}}) #변수로 가져온 숫자는 int()로 숫자로 바꿔줘야함
+    return jsonify({'msg': '버킷 완료'})
+
+#버킷 완료를 취소
+@app.route("/bucket/cancle", methods=["POST"])
+def bucket_cancle():
+    num_receive = request.form['num_give']
+    db.bucket.update_one({'num': int(num_receive)}, {'$set': {'done': 0}}) #변수로 가져온 숫자는 int()로 숫자로 바꿔줘야함
+    return jsonify({'msg': '버킷 취소'})
+
+#버킷을 삭제
+@app.route("/bucket/delete", methods=["POST"])
+def bucket_delete():
+    num_receive = request.form['num_give']
+    db.bucket.delete_one({'num': int(num_receive)})
+    return jsonify({'msg': '삭제 완료'})
+
+#여행장소 인포주기
+@app.route("/info", methods=["GET"])
+def info_get():
+    info_list = list(db.travel.find({}, {'_id': False}))
+    return jsonify({'info_list': info_list})
+
 @app.route('/hi', methods=["GET"])
 def trabucket_get():
     travel_list = list(db.travel.find({}, {'_id': False}))
