596ca4cf0fbf5c396ac317c80c445ff1a4b78acf
heyimcsy <cinnamon1995@naver.com>
2023-02-12 21:38:24 +0900

    html

--- login.py
+++ login.py
@@ -47,8 +47,9 @@ for trabucket in trabuckets:
 
     # db.travel.delete_one(doc)
     # db.travel.insert_one(doc)
-
-
+@app.route('/signUp', methods=["GET"])
+def signUp():
+	return "signUp.html"
 @app.route('/main')
 def main():
     return render_template('main.html')
@@ -60,4 +61,4 @@ def trabucket_get():
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=3000, debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
